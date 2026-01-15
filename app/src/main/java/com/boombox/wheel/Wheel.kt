a065c89da0fc6f0f9e75cbe36dd479e5b0b20969
boombox <229109493+wwraith711-dev@users.noreply.github.com>
2026-01-15 14:02:35 +0100

    "small wheel"

--- app/src/main/java/com/boombox/wheel/Wheel.kt
+++ app/src/main/java/com/boombox/wheel/Wheel.kt
@@ -2,11 +2,16 @@ package com.boombox.wheel
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Wheel() {
@@ -15,13 +20,18 @@ fun Wheel() {
 
     LazyColumn(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .size(width = 50.dp, height = 120.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(days.size){ index ->//days.size = 6 """ Which means """ index start count 0..6
+        items(days.size){ index ->
             Text(
-                text = days[index]//since index -> range in 0..6
+                modifier = Modifier
+                    .padding(3.dp),
+                text = days[index],
+                fontSize = 20.sp,
+                color = Color.White
             )
         }
     }
