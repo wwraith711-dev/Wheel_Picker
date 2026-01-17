ce1c922fd4344ca80cabc4e5b817f1f86dd10bcb
boombox <229109493+wwraith711-dev@users.noreply.github.com>
2026-01-17 05:39:50 +0100

    "opacity future "

--- app/src/main/java/com/boombox/wheel/Wheel.kt
+++ app/src/main/java/com/boombox/wheel/Wheel.kt
@@ -13,6 +13,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,18 +30,20 @@ fun Wheel() {
 
     val days = listOf("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
 
+    //2) infinite Scrolling Wheels
     LaunchedEffect(Unit) {
-        scope.launch { state.scrollToItem(days.size*100/2) }
+        scope.launch {
+            state.scrollToItem(days.size*100/2,(-state.layoutInfo.viewportSize.height/2))//add scroll half list top exactly to center of huge list
+        }
     }
-
-    //1) here we make after every scroll it center the item to center
+    //3) snap-to-Center Behavior
     LaunchedEffect(state.isScrollInProgress) {
-        if (!state.isScrollInProgress) {// after lift the finger
-            val mid = state.layoutInfo.viewportSize.height/2 // this the mid of the view part of list
-            val closetItem = state.layoutInfo.visibleItemsInfo.minByOrNull {// minimum item who has smallest distance from mid
-                abs(it.offset +(it.size/2)- mid) // distance from mid
+        if (!state.isScrollInProgress) {
+            val mid = state.layoutInfo.viewportSize.height/2
+            val closetItem = state.layoutInfo.visibleItemsInfo.minByOrNull {
+                abs(it.offset +(it.size/2)- mid)
             }?: return@LaunchedEffect
-            scope.launch { state.scrollBy( //auto scroll it smallest distance item to center
+            scope.launch { state.scrollBy(
                 (closetItem.offset +(closetItem.size/2) - mid).toFloat()
             ) }
         }
@@ -46,12 +52,34 @@ fun Wheel() {
         state = state,
         modifier = Modifier
             .fillMaxWidth()
-            .size(width = 50.dp, height = 120.dp),
+            .size(width = 50.dp, height = 120.dp)
+            .drawWithContent{//3) we create some layouts for middle item
+                drawContent()//our content
+                drawRect(//top alpha
+                    topLeft = Offset(0f,size.height/2),
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.Transparent,Color.Black),
+                        startY = size.height/2,
+                        endY = size.height
+                    )
+                )
+                drawRect(//bottom alpha
+                    topLeft = Offset(0f,0f),
+                    size = Size(size.width,size.height/2),
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.Black,Color.Transparent),
+                        startY = 0f,
+                        endY = size.height/2
+                    )
+                )
+            },
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        //1) Infinite list
         items(days.size*100){ i ->
             val index = i % days.size
+
             Text(
                 modifier = Modifier
                     .padding(3.dp),
