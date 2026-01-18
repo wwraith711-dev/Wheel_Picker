919b33acb7a76747fbd379d0647e7597e2daa534
boombox <229109493+wwraith711-dev@users.noreply.github.com>
2026-01-18 02:12:48 +0100

    "Wheel 1.0 "

--- app/src/main/java/com/boombox/wheel/Wheel.kt
+++ app/src/main/java/com/boombox/wheel/Wheel.kt
@@ -1,16 +1,14 @@
 package com.boombox.wheel
 
-import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
@@ -24,38 +22,46 @@ import kotlinx.coroutines.launch
 import kotlin.math.abs
 
 @Composable
-fun Wheel() {
+fun Wheel(
+    list: List<String>,
+    currentIndex: Int = 0,
+    selectedIndex: (Int) -> Unit
+) {
     val state = rememberLazyListState()
     val scope = rememberCoroutineScope()
 
-    val days = listOf("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
-
-    //2) infinite Scrolling Wheels
     LaunchedEffect(Unit) {
         scope.launch {
-            state.scrollToItem(days.size*100/2,(-state.layoutInfo.viewportSize.height/2))//add scroll half list top exactly to center of huge list
+            state.animateScrollToItem(currentIndex + list.size*100,(-state.layoutInfo.viewportSize.height/2))
         }
     }
-    //3) snap-to-Center Behavior
     LaunchedEffect(state.isScrollInProgress) {
-        if (!state.isScrollInProgress) {
+        if (!state.isScrollInProgress ) {
             val mid = state.layoutInfo.viewportSize.height/2
             val closetItem = state.layoutInfo.visibleItemsInfo.minByOrNull {
                 abs(it.offset +(it.size/2)- mid)
             }?: return@LaunchedEffect
-            scope.launch { state.scrollBy(
+            scope.launch { state.animateScrollBy(
                 (closetItem.offset +(closetItem.size/2) - mid).toFloat()
             ) }
+            selectedIndex(closetItem.index % list.size)
         }
     }
     LazyColumn(
         state = state,
+        flingBehavior = rememberSnapFlingBehavior(state),
         modifier = Modifier
-            .fillMaxWidth()
-            .size(width = 50.dp, height = 120.dp)
-            .drawWithContent{//3) we create some layouts for middle item
-                drawContent()//our content
-                drawRect(//top alpha
+            .size(width = 50.dp, height = 150.dp)
+            .drawWithContent{
+                drawContent()
+                drawRect(
+                    size = Size(size.width,size.height/2),
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.Black,Color.Transparent),
+                        endY = size.height/2
+                    )
+                )
+                drawRect(
                     topLeft = Offset(0f,size.height/2),
                     brush = Brush.verticalGradient(
                         colors = listOf(Color.Transparent,Color.Black),
@@ -63,27 +69,16 @@ fun Wheel() {
                         endY = size.height
                     )
                 )
-                drawRect(//bottom alpha
-                    topLeft = Offset(0f,0f),
-                    size = Size(size.width,size.height/2),
-                    brush = Brush.verticalGradient(
-                        colors = listOf(Color.Black,Color.Transparent),
-                        startY = 0f,
-                        endY = size.height/2
-                    )
-                )
             },
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        //1) Infinite list
-        items(days.size*100){ i ->
-            val index = i % days.size
-
+        items(list.size*200){ i ->
+            val index = i % list.size
             Text(
                 modifier = Modifier
                     .padding(3.dp),
-                text = days[index],
+                text = list[index],
                 fontSize = 20.sp,
                 color = Color.White
             )
